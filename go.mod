3c32dd134067172fc188abb614f8f1045bb5c984
haguru <marc.p.fuller@gmail.com>
2022-04-05 22:55:35 -0700

    wip
    Signed-off-by: haguru <marc.p.fuller@gmail.com>

--- go.mod
+++ go.mod
@@ -59,7 +59,10 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gocql/gocql v1.0.0 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.2.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,71 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/gocql/gocql"
 )
 
+type DBObj struct {
+	Id   gocql.UUID
+	Text string
+}
+
 func main() {
-	fmt.Println("WIP")
+	// fmt.Println("WIP")
+	// TODO: TAKE A LOOK AT GOCQLX FOR STRACT TAGS
+	// api needs to wait a bit for cassandra to come up thus, retries need to implemented
+	cluster := gocql.NewCluster("172.17.0.3")
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create keyspace
+	if err := session.Query("create keyspace example with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }").Exec(); err != nil {
+		fmt.Println("Error while inserting example")
+		fmt.Println(err)
+	}
+	if err := session.Query("create table example.tweet(timeline text, id UUID, text text, PRIMARY KEY(id))").Exec(); err != nil {
+		fmt.Println("Error while creating table example.tweet")
+		fmt.Println(err)
+	}
+	if err := session.Query("create index on example.tweet(timeline)").Exec(); err != nil {
+		fmt.Println("Error while creating index")
+		fmt.Println(err)
+	}
+
+	// create index on example.tweet(timeline);
+	session.Close()
+	// SELECT THE CREATED KEYSPACE
+	cluster.Keyspace = "example"
+
+	cluster.Consistency = gocql.Quorum
+	session2, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session2.Close()
+
+	if err = session2.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
+		"me", gocql.TimeUUID(), "hello world").Exec(); err != nil {
+		log.Fatal(err)
+	}
+
+	var id gocql.UUID
+	var text string
+	// testVar := &DBObj{}
+
+	if err := session2.Query(`SELECT id, text FROM tweet WHERE timeline = ? LIMIT 1`,
+		"me").Consistency(gocql.One).Scan(&id, &text); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Tweet:", id, text)
+
+	iter := session2.Query(`SELECT id, text FROM tweet WHERE timeline = ?`, "me").Iter()
+	for iter.Scan(&id, &text) {
+		fmt.Println("Tweet:", id, text)
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
